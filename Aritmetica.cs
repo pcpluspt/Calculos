e189f74a45b6105603162e08a192f0e68846986b
pcpluspt <pcplus.pt@gmail.com>
2024-01-29 17:42:36 +0000

    Create Aritmetica.cs
    Adicionei função Somar

---
+++ Aritmetica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    internal class Aritmetica
+    {
+        public int Somar(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
